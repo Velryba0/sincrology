ae4e20cbca800df6b81eab784c5d37bec0373ada
Fernando <Sabaiz>
2019-12-01 15:50:20 -0600

    Corrección al menú

--- src/layout/Menu.js
+++ src/layout/Menu.js
@@ -7,10 +7,12 @@ import '../sass/menu.styles.scss';
 const Menu = () => {
     return (
         <div className='menu'>
-            <img src={logo} alt='logo' className='logo' />
-            <a className='menu-buyEnvironments' >COMPRA POR AMBIENTES</a>
-            <a className='menu-branch' >SUCURSALES</a>
-            <a className='menu-loyalty' >LOYALTY PROGRAM</a>
+            <div className='menu-container'>
+                <img src={logo} alt='logo' className='logo' />
+                <a className='menu-buyEnvironments' >COMPRA POR AMBIENTES</a>
+                <a className='menu-branch' >SUCURSALES</a>
+                <a className='menu-loyalty' >LOYALTY PROGRAM</a>
+            </div>
             <a className='menu-livingRoom' >SALA</a>
             <a className='menu-diningRoom' >COMEDOR</a>
             <a className='menu-bedroom' >RECAMARA</a>
