6a79d2f6049de139434bfcc891b3d92f89c68c93
Velryba <fernando,sabaiz@gmail.com>
2019-11-30 18:32:00 -0600

    Menu finalizado

--- src/layout/Menu.js
+++ src/layout/Menu.js
@@ -8,9 +8,17 @@ const Menu = () => {
     return (
         <div className='menu'>
             <img src={logo} alt='logo' className='logo' />
-            <a className='search'/>
-            <a className='login'/>
-            <a className='shop'/>
+            <a className='menu-buyEnvironments' >COMPRA POR AMBIENTES</a>
+            <a className='menu-branch' >SUCURSALES</a>
+            <a className='menu-loyalty' >LOYALTY PROGRAM</a>
+            <a className='menu-livingRoom' >SALA</a>
+            <a className='menu-diningRoom' >COMEDOR</a>
+            <a className='menu-bedroom' >RECAMARA</a>
+            <a className='menu-carpet' >TAPETES</a>
+            <a className='menu-external' >EXTERIOR</a>
+            <a className='menu-search'/>
+            <a className='menu-login'/>
+            <a className='menu-shop'/>
         </div>
     )
 }
